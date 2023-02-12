67d32854050b153f0247970ebcc896a32d1080e1
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-02-12 22:34:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
-SCRIPTPATH=$( dirname -- ${BASH_SOURCE[0]}; );
+rel_SCRIPTPATH=$( dirname -- ${BASH_SOURCE[0]}; );
+
+# The function below was copied from:
+# https://stackoverflow.com/questions/3915040/how-to-obtain-the-absolute-path-of-a-file-via-shell-bash-zsh-sh
+function abspath() {
+    # generate absolute path from relative path
+    # $1     : relative filename
+    # return : absolute path
+    if [ -d "$1" ]; then
+        # dir
+        (cd "$1"; pwd)
+    elif [ -f "$1" ]; then
+        # file
+        if [[ $1 = /* ]]; then
+            echo "$1"
+        elif [[ $1 == */* ]]; then
+            echo "$(cd "${1%/*}"; pwd)/${1##*/}"
+        else
+            echo "$(pwd)/$1"
+        fi
+    fi
+}
+
+SCRIPTPATH=$(abspath $rel_SCRIPTPATH);
 
 mkdir -p $HOME/.batchconvert/bin;
 chmod -R 777 $HOME/.batchconvert;
@@ -21,3 +44,4 @@ fi;
 
 source $HOME/.bashrc;
 source $HOME/.bashrc;
+
