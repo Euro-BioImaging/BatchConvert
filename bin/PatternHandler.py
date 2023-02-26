513c5c5f744ef08d6024edb0d4dde7d1c261a6bc
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-02-26 18:01:46 +0100

    Add files via upload

--- bin/PatternHandler.py
+++ bin/PatternHandler.py
@@ -1,175 +1,107 @@
 #!/usr/bin/env python
-import os
-import pandas as pd
-import numpy as np
+import os, re
 import shutil
+import copy
 
-mask_func = lambda x: len(np.unique(x)) == 1
-
-def _separate_groups(series):
-    if not isinstance(series, type([])):
-        try:
-            series = list(series)
-        except:
-            raise TypeError('Input cannot be converted into a list type.')
-    assert isinstance(series, type([]))
-    series.sort()
-    groups = []
-    group = []
-    for i in range(1, len(series)):
-        col0 = series[i - 1]
-        col1 = series[i]
-        if col1 - col0 == 1:
-            group.append(col0)
+def save_pattern_file(rootDir, concatenation_axes = 'infer_from_filenames'):
+    assert isinstance(concatenation_axes, type('')), 'concatenation_axes must be in type str'
+    assert isinstance(rootDir, type('')), 'rootDir must be in type str'
+    filelist = os.listdir(rootDir)
+    filelist.sort()
+    last = filelist[-1]
+    zonelists = []
+    zonelist_ids = []
+    for file in filelist:
+        # zonelist = list(map(int, re.findall(r'\d+', file)))
+        # zoneids = [(m.start(0), m.end(0)) for m in re.finditer(r'\d+', file)]
+        zonelist, zonelistidx = [], []
+        for m in re.finditer(r'\d+', file):
+            zoneidx = (m.start(0), m.end(0))
+            zone = file[zoneidx[0]:zoneidx[1]]
+            zonelistidx.append(zoneidx)
+            zonelist.append(zone)
+        zonelist_ids.append(zonelistidx)
+        zonelists.append(zonelist)
+    zonelistlen = len(zonelist)
+    zonelistdict = {}
+    zonelist_ids_dict = {}
+    for i in range(zonelistlen):
+        zonelistdict[i] = []
+        zonelist_ids_dict[i] = []
+    for zonelist, zonelistidx in zip(zonelists, zonelist_ids):
+        for i in range(zonelistlen):
+            zonelistdict[i].append(zonelist[i])
+            zonelist_ids_dict[i].append(zonelistidx[i])
+    variable_zones = []
+    patterns = []
+    variable_zone_ids = []
+    reg = last
+    for i in range(zonelistlen):
+        dictitem = zonelistdict[i]
+        dictitem.sort()
+        minval = dictitem[0]
+        maxval = dictitem[-1]
+        zonelist_ids = zonelist_ids_dict[i]
+        if minval == maxval:
+            pass
         else:
-            if (col0 - 1) in group:
-                group.append(col0)
-                groups.append(group)
-                group = []
-            else:
-                groups.append([col0])
-    if (col1 - 1) in group:
-        group.append(col1)
-        groups.append(group)
-        group = []
+            variable_zones.append((minval, maxval))
+            pattern = '<%s-%s>' % (minval, maxval)
+            variable_zone_ids.append(zonelist_ids[-1])
+            patterns.append(pattern)
+
+    ### Make sure that the user inserted the fitting number of concatenation axes
+    if concatenation_axes != 'infer_from_filenames':
+        assert len(concatenation_axes) == len(patterns), "The specified number of axes does not match the number of detected variable zones, which is %s." % len(patterns)
+    ### Modify the pattern files to make sure the user-specified axes are inserted
+        for i in range(len(patterns)):
+            patterns[i] = '%s' % concatenation_axes[i].capitalize() + patterns[i]
+
+    ### Create the regex according to the detected variable zones
+    oldreg = reg
+    diff = 0
+    for pattern, variable_zone_idx in zip(patterns, variable_zone_ids):
+        idx0, idx1 = variable_zone_idx
+        idx0 += diff; idx1 += diff
+        reg = reg[:idx0] + pattern + reg[idx1:]
+        diff = len(reg) - len(oldreg)
+        oldreg = reg
+
+    #################################################################################################################################
+    ################ So the pattern file is thus generated. Now, the real file names must be modified if needed #####################
+    #################################################################################################################################
+
+    if concatenation_axes == 'infer_from_filenames':
+        newDir = rootDir
     else:
-        groups.append([col1])
-    return groups
+        newfilelist = copy.deepcopy(filelist)
+        for i, file in enumerate(newfilelist):
+            oldfile = file
+            diff = 0
+            for j, variable_zone_idx in enumerate(variable_zone_ids):
+                idx0, idx1 = variable_zone_idx
+                idx0 += diff
+                idx1 += diff
+                listing = list(file)
+                listing.insert(idx0, concatenation_axes[j].capitalize())
+                file = ''.join(listing)
+                diff = len(file) - len(oldfile)
+                oldfile = file
+            newfilelist[i] = file
+        assert len(newfilelist) == len(filelist), 'newfilelist and filelist do not have the same length.'
 
-def _aggregate_group(group):
-    arr = group.to_numpy().astype(int)
-    columns = list(group.columns)
-    cols = np.array(columns)
-    powers = np.power(10, np.arange(len(cols)))[::-1]
-    powers = powers.reshape(1, -1)
-    aggregated = (arr * powers).sum(axis = 1)
-    newgroup = group.copy(deep = True)
-    newgroup.iloc[:, :-1] = None
-    newgroup.iloc[:, -1] = aggregated
-    return newgroup
-
-def _aggregate_groups(df):
-    columns = df.columns
-    groups = _separate_groups(columns)
-    parts = [df.loc[:, tuple(gr)] for gr in groups]
-    parts_aggregated = [_aggregate_group(gr) for gr in parts]
-    concatted = pd.concat(parts_aggregated, axis = 1)
-    return concatted
-
-# agg = _aggregate_groups(nonmatch)
-
-class PatternHandler:
-    def __init__(self, rootDir, force_pattern = None):
-        filenames = os.listdir(rootDir)
-        filenames.sort()
-        df_rootDir = pd.DataFrame([list(item) for item in filenames])
-        self.rootDir = rootDir
-        self.filenames = filenames
-        self.df_rootDir = df_rootDir
-        self.forced_pattern = force_pattern
-    def _infer_pattern(self): ### DETECT ALSO MULTIPLE DIMENSIONALITIES, NOT JUST ONE
-        df = self.df_rootDir
-        # df = df_rootDir
-        pattern_mask = df.apply(mask_func, axis = 0)
-        nonmatch = df.loc[:, pattern_mask == False]
-        nonmatch = _aggregate_groups(nonmatch)
-        nonmatch_mins = nonmatch.min(axis = 0)
-        nonmatch_maxs = nonmatch.max(axis = 0)
-        masked = df.loc[:, pattern_mask]
-        for i in nonmatch.columns:
-            idx0 = nonmatch_mins[i]
-            idx1 = nonmatch_maxs[i]
-            if idx0 is None and idx1 is None:
-                reg = ''
-            else:
-                idx1 = str(idx1)
-                idx0 = str(idx0)
-                for j in range(len(idx1)):
-                    if len(idx1) == len(idx0):
-                        break
-                    else:
-                        idx0 = '0' + idx0
-                reg = '<%s-%s>' % (''.join(idx0), ''.join(idx1))
-            masked[i] = reg
-        masked_sorted = masked.T.sort_index().T
-        filename_list = masked_sorted.loc[0].to_numpy().tolist()
-        filename = ''.join(filename_list)
-        return filename
-    def _insert_pattern(self, patterns):
-        df = self.df_rootDir
-        if isinstance(patterns, type('')):
-            patterns = [patterns]
-        ###
-        df_newDir = df.copy(deep = True)
-        ###
-        pattern_mask = df.apply(mask_func, axis = 0)
-        nonmatch = df.loc[:, pattern_mask == False]
-        nonmatch = _aggregate_groups(nonmatch)
-        ct_valid_columns = 0
-        for i in nonmatch.columns:
-            if nonmatch.loc[0, i] is not None:
-                ct_valid_columns += 1
-        assert len(patterns) == ct_valid_columns, 'Number of given patterns must match number of varying text fields in the filenames'
-        nonmatch_mins = nonmatch.min(axis = 0)
-        nonmatch_maxs = nonmatch.max(axis = 0)
-        masked = df.loc[:, pattern_mask]
-        df_newDir = masked.copy()
-        idx = 0
-        for i in nonmatch.columns:
-            idx0 = nonmatch_mins[i]
-            idx1 = nonmatch_maxs[i]
-            if idx0 is None and idx1 is None:
-                reg = ''
-            else:
-                idx1 = str(idx1)
-                idx0 = str(idx0)
-                for j in range(len(idx1)):
-                    if len(idx1) == len(idx0):
-                        break
-                    else:
-                        idx0 = '0' + idx0
-                idx_infile = []
-                for item in nonmatch[i]:
-                    itemstr = str(item)
-                    for j in range(len(idx1)):
-                        if len(idx1) == len(itemstr):
-                            pass
-                        else:
-                            itemstr = '0' + itemstr
-                    idx_infile.append(itemstr)
-                reg = '%s<%s-%s>' % (patterns[idx], ''.join(idx0), ''.join(idx1))
-                df_newDir[i] = [patterns[idx] + item for item in idx_infile]
-                idx += 1
-            masked[i] = reg
-        masked_sorted = masked.T.sort_index().T
-        df_newDir_sorted = df_newDir.T.sort_index().T
-        filename_list = masked_sorted.loc[0].to_numpy().tolist()
-        filename = ''.join(filename_list)
-        return df_newDir_sorted, filename
-
-    def dump_inferred_pattern(self):
-        rootDir = self.rootDir
-        pattern_file = self._infer_pattern()
-        with open(rootDir + '/new.pattern', 'w') as writer:
-            writer.write(pattern_file)
-
-    def dump_inserted_pattern(self, patterns):
-        rootDir = self.rootDir
-        df_rootDir = self.df_rootDir
-        df_newDir, pattern_file = self._insert_pattern(patterns)
         newDir = rootDir + '/tempdir'
+
         try:
             os.makedirs(newDir)
         except:
             pass
-        lol_rootDir = df_rootDir.to_numpy().tolist()
-        lol_newDir = df_newDir.to_numpy().tolist()
-        for olditem, newitem in zip(lol_rootDir, lol_newDir):
-            oldfile = ''.join(olditem)
-            newfile = ''.join(newitem)
-            oldpath = os.path.join(rootDir, oldfile)
-            newpath = os.path.join(newDir, newfile)
-            shutil.copy(oldpath, newpath)
-        with open(newDir + '/new.pattern', 'w') as writer:
-            writer.write(pattern_file)
 
+        for olditem, newitem in zip(filelist, newfilelist):
+            oldpath = os.path.join(rootDir, olditem)
+            newpath = os.path.join(newDir, newitem)
+            shutil.copy(oldpath, newpath)
+    with open(newDir + '/new.pattern', 'w') as writer:
+        writer.write(reg)
+
+    return reg
