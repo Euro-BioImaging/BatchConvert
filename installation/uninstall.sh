8b16c9917900e9674f862862f8ef3cb9e85fdac8
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-03-07 16:49:04 +0000

    Add files via upload

--- installation/uninstall.sh
+++ installation/uninstall.sh
@@ -1,30 +1,11 @@
 #!/usr/bin/env bash
 
 rel_SCRIPTPATH=$( dirname -- ${BASH_SOURCE[0]}; );
-
-# https://stackoverflow.com/questions/3915040/how-to-obtain-the-absolute-path-of-a-file-via-shell-bash-zsh-sh
-function abspath() {
-    # generate absolute path from relative path
-    # $1     : relative filename
-    # return : absolute path
-    if [ -d "$1" ]; then
-        # dir
-        (cd "$1"; pwd)
-    elif [ -f "$1" ]; then
-        # file
-        if [[ $1 = /* ]]; then
-            echo "$1"
-        elif [[ $1 == */* ]]; then
-            echo "$(cd "${1%/*}"; pwd)/${1##*/}"
-        else
-            echo "$(pwd)/$1"
-        fi
-    fi
-}
+source $rel_SCRIPTPATH/../bin/utils.sh
 
 SCRIPTPATH=$(abspath $rel_SCRIPTPATH)/..;
-#printf SCRIPTPATH
 chmod -R 777 $SCRIPTPATH;
+
 
 cat $HOME/.bashrc | grep -v BatchConvert > $HOME/bashrc_replace;
 cat $HOME/bashrc_replace > $HOME/.bashrc;
