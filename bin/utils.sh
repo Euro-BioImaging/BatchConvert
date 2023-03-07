0c35ceeeb2b810f5db11eff9e4699a4d1ed2286b
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-03-07 16:48:32 +0000

    Add files via upload

---
+++ bin/utils.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# https://stackoverflow.com/questions/3915040/how-to-obtain-the-absolute-path-of-a-file-via-shell-bash-zsh-sh
+
+function abspath() {
+    # generate absolute path from relative path
+    # $1     : relative filename
+    # return : absolute path
+    if [ -d "$1" ]; then
+        # dir
+        (cd "$1"; pwd)
+    elif [ -f "$1" ]; then
+        # file
+        if [[ $1 = /* ]]; then
+            echo "$1"
+        elif [[ $1 == */* ]]; then
+            echo "$(cd "${1%/*}"; pwd)/${1##*/}"
+        else
+            echo "$(pwd)/$1"
+        fi
+    fi
+}
+
