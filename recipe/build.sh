a139a7dcd93f00e91d2fbee2616d6f9b18adce85
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-08-20 15:20:28 +0000

    add files

---
+++ recipe/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+tempdir=$PREFIX/lib/${PKG_NAME}-${PKG_VERSION};
+echo "bu ne lan kari gibi, $ROOT"
+echo $ROOT
+mkdir -p $tempdir;
+cp -r $SRC_DIR/* $tempdir;
+chmod -R 777 $tempdir
+
+echo "#!/usr/bin/env bash" > $PREFIX/bin/batchconvert; 
+echo 'SCRIPTPATH=$( dirname -- ${BASH_SOURCE[0]}; );' >> $PREFIX/bin/batchconvert; 
+script="\$SCRIPTPATH/../lib/${PKG_NAME}-${PKG_VERSION}/batchconvert.sh "\$@"";
+echo $script >> $PREFIX/bin/batchconvert; 
+chmod +x $PREFIX/bin/batchconvert
+
