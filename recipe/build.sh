8fe6fa267a31546f8c51aae7063f667d97f66354
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-08-25 11:57:27 +0000

    Add files via upload

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,8 @@ chmod -R 777 $tempdir
 
 echo "#!/usr/bin/env bash" > $PREFIX/bin/batchconvert; 
 echo 'SCRIPTPATH=$( dirname -- ${BASH_SOURCE[0]}; );' >> $PREFIX/bin/batchconvert; 
-script="\$SCRIPTPATH/../lib/${PKG_NAME}-${PKG_VERSION}/batchconvert.sh "\$@"";
+part1="\$SCRIPTPATH/../lib/${PKG_NAME}-${PKG_VERSION}/batchconvert.sh"
+part2=$'\x22$@\x22';
+script=$part1' '$part2
 echo $script >> $PREFIX/bin/batchconvert; 
 chmod +x $PREFIX/bin/batchconvert
