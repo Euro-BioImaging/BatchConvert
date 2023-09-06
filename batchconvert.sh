f8135ff1f861d2d2d4a2980b215a9c25e9db9be1
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-09-06 08:06:52 +0000

    batchconvert.sh update error messages

--- batchconvert.sh
+++ batchconvert.sh
@@ -6,7 +6,7 @@ SCRIPTPATH=$( dirname -- ${BASH_SOURCE[0]}; );
 source $SCRIPTPATH/bin/utils.sh
 
 set -f && \
-result=$(python $SCRIPTPATH/bin/parameterise_conversion.py "$@";)
+result=$(pythonexe $SCRIPTPATH/bin/parameterise_conversion.py "$@";)
 
 if [[ -f $SCRIPTPATH/bin/.process ]];
   then
@@ -71,8 +71,8 @@ elif [[ $process == 'converted' ]];
   then
     cd $SCRIPTPATH/bin && \
 
-    python construct_cli.py > batchconvert_cli.sh && \
-    python construct_nextflow_cli.py > nextflow_cli.sh && \
+    pythonexe construct_cli.py > batchconvert_cli.sh && \
+    pythonexe construct_nextflow_cli.py > nextflow_cli.sh && \
     printf "${GREEN}Nextflow script has been created. Workflow is beginning.\n${NORMAL}"
     cd - && \
 
@@ -89,7 +89,7 @@ if [[ $1 == "ometiff" ]] || [[ $1 == "omezarr" ]];
     if [[ $afterrun != "noclean" ]];
       then
         # echo $afterrun
-        python $SCRIPTPATH/bin/clean_workdir.py;
+        pythonexe $SCRIPTPATH/bin/clean_workdir.py;
     fi
 fi
 
@@ -98,7 +98,7 @@ if [[ -f $SCRIPTPATH/bin/.afterrun ]];
   rm $SCRIPTPATH/bin/.afterrun
 fi
 
-python $SCRIPTPATH/bin/cleanup.py &> /dev/null
+pythonexe $SCRIPTPATH/bin/cleanup.py &> /dev/null
 
 
 
