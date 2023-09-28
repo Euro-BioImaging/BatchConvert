4cb5ee290bda5f27c0629d09a5a1ef789ddac20b
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-09-28 18:37:44 +0200

    fix interactive configuration issue

--- bin/parameterise_conversion.py
+++ bin/parameterise_conversion.py
@@ -4,8 +4,16 @@ from subprocess import PIPE
 if __name__ == "__main__":
     scriptpath = os.path.dirname(os.path.realpath(__file__))
     cmd = [os.path.join(scriptpath, "batchconvert"), *sys.argv[1:]]
-    result = subprocess.run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    if len(result.stderr) == 0:
-        print(result.stdout)
+    interactive_commands = ['configure_s3_remote', 'configure_ometiff', 'configure_omezarr', 'configure_slurm']
+    if cmd[1] in interactive_commands:
+        proc = subprocess.Popen(cmd, universal_newlines=True)
+        _ = proc.communicate()
     else:
-        print(result.stderr)
+        proc = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        out, error = proc.communicate()
+        if len(error) == 0:
+            with open(os.path.join(scriptpath, '.stdout'), 'w') as writer:
+                writer.write(out)
+        else:
+            with open(os.path.join(scriptpath, '.stderr'), 'w') as writer:
+                writer.write(error)
