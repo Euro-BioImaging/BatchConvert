2eb36acf6d19b6cfa2c7a7d5b79143549fda2d3e
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-10-01 17:45:48 +0200

    multiple updates

---
+++ parameterise_conversion.py
@@ -0,0 +1,19 @@
+import os, json, subprocess, sys
+from subprocess import PIPE
+
+if __name__ == "__main__":
+    scriptpath = os.path.dirname(os.path.realpath(__file__))
+    cmd = [os.path.join(scriptpath, "batchconvert"), *sys.argv[1:]]
+    interactive_commands = ['configure_s3_remote', 'configure_ometiff', 'configure_omezarr', 'configure_slurm']
+    if len(cmd) == 2 and cmd[1] in interactive_commands:
+        proc = subprocess.Popen(cmd, universal_newlines=True)
+        _ = proc.communicate()
+    else:
+        proc = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        out, error = proc.communicate()
+        if len(error) == 0:
+            with open(os.path.join(scriptpath, '.stdout'), 'w') as writer:
+                writer.write(out)
+        else:
+            with open(os.path.join(scriptpath, '.stderr'), 'w') as writer:
+                writer.write(error)
