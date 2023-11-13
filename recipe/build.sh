b0535249f9f4e829fbeec00af3ad2ad5a66e5105
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2023-11-13 13:35:09 +0100

    update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -eu
-tempdir=$PREFIX/lib/${PKG_NAME}-${PKG_VERSION};
+tempdir=$PREFIX/share/${PKG_NAME}-${PKG_VERSION};
 mkdir -p $tempdir;
 cp -r $SRC_DIR/* $tempdir;
 chmod -R 777 $tempdir
 
 echo "#!/usr/bin/env bash" > $PREFIX/bin/batchconvert; 
 echo 'SCRIPTPATH=$( dirname -- ${BASH_SOURCE[0]}; );' >> $PREFIX/bin/batchconvert; 
-part1="\$SCRIPTPATH/../lib/${PKG_NAME}-${PKG_VERSION}/batchconvert.sh"
+part1="\$SCRIPTPATH/../share/${PKG_NAME}-${PKG_VERSION}/batchconvert.sh"
 part2=$'\x22$@\x22';
 script=$part1' '$part2
 echo $script >> $PREFIX/bin/batchconvert; 
